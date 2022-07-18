e8c11c609f9f667c0568bb0dbe47a6bed4fc360c
manduakash <50767686+manduakash@users.noreply.github.com>
2022-07-18 16:06:46 +0530

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -5,12 +5,12 @@
 package com.innovation.shapes;
 import java.util.Scanner;
 
-public class Circle {
-	float radius;
+public class Circle {	//class
+	public float radius;
 	public float pi=3.5f;
 	
-	public Circle() {		// constructor 1
-		radius=1.5f;
+	public Circle() {		// constructor 1 ; setting default value
+		this.radius=1.5f;
 	}
 	public Circle(float radius) {		// constructor 2
 		this.radius=radius;
@@ -20,23 +20,29 @@ public class Circle {
 		this.pi=3.5f;
 	}
 	
-	public float calculateCircleArea(float radius) {
+	public float calculateCircleArea(float radius) {	//method for calculating area
 		return pi*radius*radius;
 	}
-	public float calculateCircumference(float radius) {
+	public float calculateCircumference(float radius) {		//method for calculating circumference
 		return 2*pi*radius;
 	}
 	
 	//main method
 	public static void main(String[] args) {
+		
+		// instantiating objects
 		Circle obj = new Circle();
-		Scanner scan = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);		// for taking input
+		
+		// taking user input
 		System.out.println("Input your radius");
 		float a = scan.nextFloat();
 		
-		System.out.println("The area of circle is "+ obj.calculateCircleArea(a));
-		System.out.println("The circumference of circle is "+ obj.calculateCircumference(a));
-		scan.close();
+		// invoking the methods printing as ouput
+		System.out.println("The area of circle is "+ obj.calculateCircleArea(a));		// for area 
+		System.out.println("The circumference of circle is "+ obj.calculateCircumference(a));	// for circumference
+		
+		scan.close();	// closing input strean
 		
 	}
 }
