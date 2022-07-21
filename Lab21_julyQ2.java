59cca7cb802e87c6e661945b16b3f7ad3ad63767
manduakash <50767686+manduakash@users.noreply.github.com>
2022-07-21 19:38:05 +0530

    Update Lab21_julyQ2.java

--- Lab21_julyQ2.java
+++ Lab21_julyQ2.java
@@ -1,37 +1,49 @@
 //@author Akash Singh
-//Lab 21 july Question no. 2
+//Lab 21 july Question no. 2:- Find the total number of duplicates in array
 package Lab;
+import java.util.Scanner;
 
-public class Lab21_julyQ2 {
-
-	public static void main(String[] args) {
+public class Lab21_julyQ2 {		//class
+	public static void main(String[] args) {	//main method
+		
+		Scanner scan = new Scanner(System.in);					//instantiating scanner object 
+		
+		System.out.println("Enter the size of array: ");
+		int n = scan.nextInt();							//for array size
 		
 		// instantiating and initializing array
-				int[] arr = {5,2,7,7,5}; 
-				
-				// logic
-				int duplicate = 0;				// it is a counter variable
-				
-				lebelOuter:
-				for(int i = 0; i < arr.length; i++) {	//loop for searching
-					for(int j=i+1 ;j < arr.length; j++) {
-						if(arr[i] == arr[j]) {		// condition checking
-							duplicate=1;				
-							break lebelOuter;			// label break will terminate outer loop
-						}
-						else {
-							duplicate=0;					// if not found the value assigned 0
-						}
-					}
+		int[] arr = new int[n]; 
+		
+		
+		System.out.println("Enter elements of array: ");
+		for(int i=0; i<n; i++) {
+			arr[i]=scan.nextInt();						//taking array elements from user
+			}
+			
+			
+		// logic
+		int duplicate = 0;							// counter variable
+
+		for(int i = 0; i < arr.length; i++) {					//loop for one element to compare
+
+			for(int j=i+1; j < arr.length; j++) {				//loop for checking one by one to all array items
+
+				if(arr[i] == arr[j]) {					//condition checking
+					duplicate++;					//incrementing counter varible
+					break;
 				}
-				
-				System.out.println("Is there any redundant element in this array: ");
-				// checking counter variable
-				if(duplicate==1) {		
-					System.out.println("Duplicate found");		// if found
-				}else {
-					System.out.println("Duplicate not found");	// if not found
-				}
+			}
+		}
+
+
+		// checking counter variable
+		if(duplicate>=1) {							// if found
+			System.out.println("\nTotal "+duplicate+" Duplicates found");	// SOP
+		}else {
+			System.out.println("Duplicate not found");			// if not found
+		}
+
+		scan.close();								//closing input stream
 	}
 
 }
