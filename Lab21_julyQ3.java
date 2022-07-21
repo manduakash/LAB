1f307a1bbf252a589b79b830fd6e28252716f361
manduakash <50767686+manduakash@users.noreply.github.com>
2022-07-21 20:21:41 +0530

    Update Lab21_julyQ3.java

--- Lab21_julyQ3.java
+++ Lab21_julyQ3.java
@@ -1,17 +1,17 @@
 //@author Akash Singh
-//Lab 21 july Question no. 3
+//Lab 21 july Question no. 3:- Print elements of the array odd position-wise.
 package Lab;
 
 public class Lab21_julyQ3 {
 	public static void main(String[] args) {	//main method
 		
 		// instantiating and initializing array
-				String[] arr = {"a","b","c","d","e"}; 
-				
-				// logic
-				System.out.println("Elements that are present in odd positions: ");
-				for (int i = 0; i < arr.length; i=i+2) {	//loop for traversing odd position-wise not odd index-wise
-					System.out.print(arr[i]+" ");		// SOP of each element
-				}
+		String[] arr = {"a","b","c","d","e"}; 
+
+		// loop for traversing odd position-wise
+		System.out.println("Elements that are present in odd positions: ");
+		for (int i = 0; i < arr.length; i=i+2) {				//logic
+			System.out.print(arr[i]+" ");					// SOP of each element
+		}
 	}
 }
