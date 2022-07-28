09b8ce690044a024f71af24f0a1818828c9b3895
manduakash <50767686+manduakash@users.noreply.github.com>
2022-07-28 22:07:25 +0530

    Create LabWeek6_Q5.java

---
+++ LabWeek6_Q5.java
@@ -0,0 +1,41 @@
+/* @author AKASH SINGH 
+ * checking lowercase and underscore pattern using regular expressions
+ * */
+
+package LabWeek6;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class MyRegex2{
+	
+	void lowerUnderscore(String input) {	// static method for checking ip 
+	
+	//this is regex pattern for checking ip address
+	String pattern = "[a-z]+_[a-z]+";					     //regex pattern for this problem
+	
+	Pattern p = Pattern.compile(pattern);				     // passing regex pattern into comile method of static pattern class 
+	Matcher m = p.matcher(input);						    // matching ip agrguments to regex pattern
+			
+	if(m.matches() == true) {								// if pattern matches then it will return true 
+		System.out.println("Found a match...\n");
+	}else {													// if pattern doesnt match then it will exicute else part
+		System.out.println("Not match...\n");				
+	}
+	
+	
+  }
+}
+
+public class LabWeek6_Q5 {
+		//main method
+		public static void main(String[] args) {
+			//instantiating objects
+			MyRegex2 obj = new MyRegex2();
+			
+			//invoking lowerUnderscore method 
+			obj.lowerUnderscore("java_excercise");			//sample input 1				
+			obj.lowerUnderscore("Java_Excercise");			//sample input 2
+			
+		}
+}
