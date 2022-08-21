b16e7b21415a76f9b32df961f93e9583fa7434a4
manduakash <50767686+manduakash@users.noreply.github.com>
2022-08-21 11:07:11 +0530

    Update BookStore.java

--- BookStore.java
+++ BookStore.java
@@ -6,18 +6,18 @@ package com.book;
 import java.util.ArrayList;
 
 public class BookStore {	//main class
-	ArrayList<Book> bookList = new ArrayList<>(); 	//creating arrayList object to store book objects
+	ArrayList<Book> bookList = new ArrayList<>(); 			//creating arrayList object to store book objects
 	
 	//operation #1
 	public void addBook(Book b) {					//passing book object as argument
-		bookList.add(b);							//adding new book object to bookList Collection
+		bookList.add(b);					//adding new book object to bookList Collection
 	}
 	
 	//operation #2
-	public void searchByTitle(String title) {		//passing title string as argument to search
+	public void searchByTitle(String title) {			//passing title string as argument to search
 		for (Book b : bookList) {
-			if(title.contains(b.title)) {			//condition
-				System.out.println("bookID="+b.bookID+ " title="+b.title+" author="+b.author+" category="+b.category+ " price="+b.price); //if condition meets
+			if(title.contains(b.getTitle())) {		//condition
+				System.out.println(b);
 				break;
 			}else {
 				System.out.println("not found");
@@ -27,10 +27,10 @@ public class BookStore {	//main class
 	} 
 	
 	//operation #3
-	public void searchByAuthor(String author) {		//passing author string as argument to search
+	public void searchByAuthor(String author) {			//passing author string as argument to search
 		for (Book b : bookList) {
-			if(author.contains(b.author)) {			//condition
-				System.out.println("bookID="+b.bookID+ " title="+b.title+" author="+b.author+" category="+b.category+ " price="+b.price); //if condition meets
+			if(author.contains(b.getAuthor())) {		//condition
+				System.out.println(b);
 				break;
 			}else {
 				System.out.println("not found");
@@ -40,10 +40,10 @@ public class BookStore {	//main class
 	}
 	
 	//operation #4
-	public void displayAll() {						//displayAll method with traverse and print all the elements of bookList collection
+	public void displayAll() {					//displayAll method with traverse and print all the elements of bookList collection
 		//traversing
-		for(Book b : bookList) {					//enhanced foreach loop
-			System.out.println("bookID="+b.bookID+ " title="+b.title+" author="+b.author+" category="+b.category+ " price="+b.price);
+		for(Book b : bookList) {				//enhanced foreach loop
+			System.out.println(b);
 		}
 	}
 }
