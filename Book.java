440a36c80facedd8496471eb0b95669531bfef21
manduakash <50767686+manduakash@users.noreply.github.com>
2022-08-21 11:17:06 +0530

    Update Book.java

--- Book.java
+++ Book.java
@@ -4,16 +4,18 @@
 package com.book;
 
 public class Book {	//main class
-	String bookID, title, author, category;
-	float price;
+	
+	//encapsulating all data members
+	private String bookID, title, author, category;
+	private float price;
 	
 	public Book(String bookID, String title, String author, String category, float price)  {	//parameterized constructor
-		//validations
 		
+		//validations
 		try {
 			//conditon for bookID	
 			if((bookID.charAt(0)=='B') && (bookID.length()==4)) {	//conditon
-				this.bookID=bookID;		//if condition meet
+				this.bookID=bookID;	//if condition meet
 			}
 			else {
 				throw new InvalidBookException("book id should be start with 'B' and lenght of 4");		//throws exception
@@ -22,7 +24,7 @@ public class Book {	//main class
 			
 			//conditon for category	
 			if((category.equalsIgnoreCase("Science")) || (category.equalsIgnoreCase("Fiction")) || (category.equalsIgnoreCase("Technology")) || (category.equalsIgnoreCase("Others"))) {	//conditon
-				this.category=category;			//if condition meet
+				this.category=category;	//if condition meet
 			}
 			else {
 				throw new InvalidBookException("category should be Science,Fiction and Others");		//throws exception
@@ -30,10 +32,10 @@ public class Book {	//main class
 			
 			//conditon for price	
 			if(price>0) {	//conditon
-				this.price=price;		//if condition meet
+				this.price=price; //if condition meet
 			}
 			else {
-				throw new InvalidBookException("negative price can not be accepted");		//throws exception
+				throw new InvalidBookException("negative price can not be accepted");				//throws exception
 			}
 		this.title=title;
 		this.author=author;
@@ -42,14 +44,42 @@ public class Book {	//main class
 			System.out.println(e);
 		}
 	}
+
+	//getters for all private data members
+	public String getBookID() {
+		return bookID;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	@Override
+	public String toString() {	//for printing all details of Book objects
+		return "Book ['bookID'=" + bookID + ", 'title'=" + title + ", 'author'=" + author + ", 'category'=" + category
+				+ ", 'price'=" + price + "]";
+	}
+
+	
 }
 
 //creating custom exception class
 class InvalidBookException extends Exception{	
-	private static final long serialVersionUID = 1L;		//suggested by eclipse IDE
+	private static final long serialVersionUID = 1L;	//default serial id suggested by eclipse IDE
 
-//exception class
-
+	//overloading InvalidBookException class 
 	public InvalidBookException(String s) {	//parameterized constructor
 		super(s);	//passing 's' argument to Exception super class
 	}
